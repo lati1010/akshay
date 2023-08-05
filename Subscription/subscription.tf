16c7989acd7cc45a36f02d56120640525375153d
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2023-08-05 02:30:02 -0700

    Update subscription.tf

--- Subscription/subscription.tf
+++ Subscription/subscription.tf
@@ -1,7 +1,7 @@
-  resource "azurerm_subscription_policy_assignment" "auditvms" {
-  name = "audit-vm-manageddisks"
+  resource "azurerm_subscription_policy_assignment" "cmkkeyforvms" {
+  name = "vm-disk-cmkkey"
   subscription_id = "/subscriptions/79be621b-a188-406b-affb-2b0c00bc0d97"
-  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/497dff13-db2a-4c0f-8603-28fa3b331ab6"
+  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/702dd420-7fcc-42c5-afe8-4026edd20fe0"
   description = "Shows all virtual machines not using managed disks"
-  display_name = "Audit VMs without managed disks assignment"
+  display_name = "OS and data disks should be encrypted with a customer-managed key"
   }
