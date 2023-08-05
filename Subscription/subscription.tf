d6047a780ebc4b7adfa05af6ec2fb333f782be3e
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2023-08-05 01:31:49 -0700

    Update subscription.tf

--- Subscription/subscription.tf
+++ Subscription/subscription.tf
@@ -1,6 +1,6 @@
   resource "azurerm_subscription_policy_assignment" "auditvms" {
   name = "audit-vm-manageddisks"
-  subscription_id = var.cust_scope
+  subscription_id = "79be621b-a188-406b-affb-2b0c00bc0d97"
   policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/06a78e20-9358-41c9-923c-fb736d382a4d"
   description = "Shows all virtual machines not using managed disks"
   display_name = "Audit VMs without managed disks assignment"
