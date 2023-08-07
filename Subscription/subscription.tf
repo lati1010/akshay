e31defebc9bf0fe09ad32b41c7b811b169dc4b60
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2023-08-07 06:02:00 -0400

    Update subscription.tf

--- Subscription/subscription.tf
+++ Subscription/subscription.tf
@@ -8,7 +8,7 @@
   resource "azurerm_resource_group_policy_assignment" "cmkkeyforvmsrg" {
   name = "vm-disk-cmkkeyrg"
   resource_group_id = var.resourceid
-  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/702dd420-7fcc-42c5-afe8-4026edd20fe0"
+  policy_definition_id = var.definitionid
   description = "Shows all virtual machines not using managed disks"
   display_name = "OS and data disks should be encrypted with a customer-managed keyrg"
   }
