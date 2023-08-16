2585ea6e06494671f8ccad45204443cd9ee0c18d
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2023-08-16 06:17:13 -0400

    Update subscription.tf

--- Subscription/subscription.tf
+++ Subscription/subscription.tf
@@ -1,5 +1,5 @@
   resource "azurerm_subscription_policy_assignment" "AKSAzurePolicy" {
-  name ="${var.name}-${var.subscription_id}"
+  name = "${substr("${var.name}", 0, min(length("${var.name}"), 20))}"
   subscription_id = var.resourceid
   policy_definition_id = var.definitionid
   description = var.name
