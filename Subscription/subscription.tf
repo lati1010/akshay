b0f0d83b658712f3658a8cd10dc8b963ec339696
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2023-08-16 04:44:15 -0400

    Update subscription.tf

--- Subscription/subscription.tf
+++ Subscription/subscription.tf
@@ -1,26 +1,7 @@
   resource "azurerm_subscription_policy_assignment" "cmkkeyforvms" {
   name = "vm-disk-cmkkey"
-  subscription_id = "/subscriptions/79be621b-a188-406b-affb-2b0c00bc0d97"
-  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/702dd420-7fcc-42c5-afe8-4026edd20fe0"
+  subscription_id = var.resourceid
+  policy_definition_id = var.definitionid
   description = "Shows all virtual machines not using managed disks"
   display_name = "OS and data disks should be encrypted with a customer-managed key"
-  }
-  resource "azurerm_resource_group_policy_assignment" "cmkkeyforvmsrg" {
-  name = "vm-disk-cmkkeyrg"
-  resource_group_id = "/subscriptions/79be621b-a188-406b-affb-2b0c00bc0d97/resourceGroups/rg-test-tfg-tf-state"
-  policy_definition_id = "/providers/Microsoft.Authorization/policyDefinitions/702dd420-7fcc-42c5-afe8-4026edd20fe0"
-  description = "Shows all virtual machines not using managed disks"
-  display_name = "OS and data disks should be encrypted with a customer-managed keyrg"
-  }
-  resource "azurerm_resource_group_policy_assignment" "cmkkeyforvmsr1111g" {
-  name = "vm-disk-cmkkeyrg222"
-  resource_group_id = var.resourceid
-  policy_definition_id = var.definitionid
-  description = "Shows all virtual machines not using managed d3333isks"
-  display_name = "OS and data disks should be encrypted with a customer-managed keyr4444g"
-  }
-  resource "azurerm_management_group_policy_assignment" "example" {
-    name                 = "example-policy"
-    policy_definition_id = var.definitionid
-    management_group_id  = "716e32a2-2f56-4567-a78d-e347d95858a3"
   }
