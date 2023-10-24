cba03b6cefafa98f772330990b50f3e44226dd6d
Lavakumar Rajanala <50436618+lati1010@users.noreply.github.com>
2023-10-25 00:28:31 +0530

    Update subscription.tf

--- Subscription/subscription.tf
+++ Subscription/subscription.tf
@@ -1,7 +1,15 @@
-  resource "azurerm_subscription_policy_assignment" "AKSAzurePolicy" {
-  name = "${var.effect}-${substr("${var.name}", 0, min(length("${var.name}"), 47))}"
+resource "azurerm_subscription_policy_assignment" "akspols" {
+  for_each = {for u in var.Audit_policies : u.policy_name => u }
+  name = each.key
   subscription_id = var.resourceid
-  policy_definition_id = var.definitionid
+  policy_definition_id = each.value.def_id
   description = var.name
-  display_name = "${var.name}-${var.effect}"
-  }
+  display_name = "${var.name}"
+  parameters = jsonencode({
+    "Effect": {
+      "value": var.effect
+    }
+  })
+}
+
+
